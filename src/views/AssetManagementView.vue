607e43d5f512ea2d4e24b98f319f3a5a6b643f2b
eid iepoh <tornike.dzmanashvili@gmail.com>
2024-11-26 14:27:04 +0400

    added mobile.svg

--- src/views/AssetManagementView.vue
+++ src/views/AssetManagementView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="asset-management">
-    <h1>This is an asset management page</h1>
+    <div class="asset-management-headings">
+      <div class="asset-management-headings__header">Asset management</div>
+      <div class="asset-management-headings__route"></div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.asset-management-headings__header {
+  font-weight: 700;
+  font-size: 31.25px;
+  line-height: 38px;
+  color: #0e1629;
+}
+</style>
